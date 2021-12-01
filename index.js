dc77b5cc0ce443315c612b8a402c7c1d44dd1a2e
Austin <austinlongocontracting@gmail.com>
2021-11-30 22:20:29 -0500

    commit tree 1b3388b2c423e5e0f7f115c80b2b843d642ff011

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    let temp = location - 42;
+    if (temp < 0) {
+        return -temp
+    }
+    return temp
+}
+
+function distanceFromHqInFeet(location) {
+    return distanceFromHqInBlocks(location) * 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+    let temp = (start - end) * 264
+    if (temp < 0) {
+        return -temp
+    }
+    return temp 
+}
+
+function calculatesFarePrice (start, end) {
+    let temp = distanceTravelledInFeet(start, end);
+    if (temp <= 400) {
+        return 0;
+    }
+    else if (temp > 400 && temp < 2000) {
+        return (temp-400) * .02;
+    }
+    else if (temp > 2000 && temp <= 2500) {
+        return 25;
+    }
+    else {
+        return 'cannot travel that far';
+    } 
+}
